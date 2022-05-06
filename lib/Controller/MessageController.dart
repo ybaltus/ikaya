96e2e7783e749a1647d364c405e046bbf69c7faa
Coulibaly Karamoko <k.coulibaly@ecole-ipssi.net>
2022-05-06 18:10:50 +0200

    ajout de appBar

--- lib/Controller/MessageController.dart
+++ lib/Controller/MessageController.dart
@@ -39,25 +39,34 @@ class MessagecontrollerState extends State<Messagecontroller> {
             return const CircularProgressIndicator();
           } else {
             List<DocumentSnapshot>documents = snapshot.data!.docs;
-            return ListView.builder(
-                itemCount: documents.length,
-                itemBuilder: (BuildContext ctx,int index) {
-                  Message discussion = Message(documents[index]);
-                  if ((discussion.from == widget.id.uid &&
-                      discussion.to == widget.idPartner.uid) ||
-                      (discussion.from == widget.idPartner.uid &&
-                          discussion.to == widget.id.uid)) {
-
-                    return MessageBubble(widget.id.uid, widget.idPartner, discussion);
-                  }
-                  else {
-                    return Container();
-                  }
-                }
+            return Scaffold(
+                appBar: AppBar(
+                    title: Text("${widget.idPartner.prenom} ${widget.idPartner.nom}"),
+                ),
+                body: bodyPage(documents: documents),
             );
           }
         }
     );
   }
+
+    Widget bodyPage({required List<DocumentSnapshot<Object?>> documents}) {
+        return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (BuildContext ctx,int index) {
+                Message discussion = Message(documents[index]);
+                if ((discussion.from == widget.id.uid &&
+                    discussion.to == widget.idPartner.uid) ||
+                    (discussion.from == widget.idPartner.uid &&
+                        discussion.to == widget.id.uid)) {
+
+                    return MessageBubble(widget.id.uid, widget.idPartner, discussion);
+                }
+                else {
+                    return Container();
+                }
+            }
+        );
+    }
 }
 
